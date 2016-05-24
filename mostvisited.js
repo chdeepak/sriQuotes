877ec2e20baf7c72571653270ae34487ce0b82b6
Deepak <chdeepak@thoughtworks.com>
2016-05-25 01:49:44 +0530

    Working version but lot of clean up required

---
+++ mostvisited.js
@@ -0,0 +1,34 @@
+function onAnchorClick(event) {
+  chrome.tabs.create({ url: event.srcElement.href });
+  return false;
+}
+
+// Given an array of URLs, build a DOM list of these URLs in the
+// browser action popup.
+function buildPopupDom(mostVisitedURLs) {
+  // var popupDiv = document.getElementById('topSitesMenu');
+  // var ol = popupDiv.appendChild(document.createElement('ol'));
+  var urlNumb = "";
+
+  if(mostVisitedURLs.length < 7)
+    urlNumb = mostVisitedURLs.length;
+  else
+    urlNumb = 7;
+
+  for (var i = 0; i < urlNumb; i++) {
+    // var li = ol.appendChild(document.createElement('li'));
+    // var a = li.appendChild(document.createElement('a'));
+    var link = mostVisitedURLs[i].url;
+    var title = mostVisitedURLs[i].title;
+    if(title.length >35){
+      title= title.substring(0,32);
+      title.concat('...');
+    }
+    // $(a).append('<img src="https://plus.google.com/_/favicon?domain_url='+a.href+'">');
+    $('#sidebar ol').append('<a href="'+link+'">'+'<li><div style="color:green"><img src="https://plus.google.com/_/favicon?domain_url='+link+'">      '+title+'</div></li></a>');
+    // a.appendChild(document.createTextNode(mostVisitedURLs[i].title));
+    // a.addEventListener('click', onAnchorClick);
+  }
+}
+
+chrome.topSites.get(buildPopupDom);
--- quotes.js
+++ quotes.js
@@ -1,16 +1,24 @@
 (function(){
 var quotes = [
-    { "quoteId":"1", "quoteMessage":"Be Spiritual" },
-    { "quoteId":"2", "quoteMessage":"Seva" },
-    { "quoteId":"3", "quoteMessage":"Sadhna" }
+    { "quoteId":"1", "quoteMessage":"The only thing you must remember is how fortunate you are. When you forget this, you become sad" },
+    { "quoteId":"2", "quoteMessage":"This definite knowledge that I am not the body, I am the Self, I am the space, I am the Imperishable, untouched, untainted by the \"prakriti,\" and by this world around me; this body is all hollow and empty, and every particle in this body is changing, and changing, and changing; the mind is changing and changing and changing---this definite knowledge is the way out of the cycle." },
+    { "quoteId":"3", "quoteMessage":"When you realise everyone is a part of your Self, you enjoy the whole world without a sense of craving" }
     ];
   var images = [
-    { "imageId":"1", "uri":"/srisri.png" },
-    { "imageId":"2", "uri":"/aol.png" },
-    { "imageId":"3", "uri":"/happy.png" }
+    { "imageId":"1", "uri":"/srisri.jpeg" },
+    { "imageId":"2", "uri":"/aol.jpeg" },
+    { "imageId":"3", "uri":"/happy.jpeg" },
+        { "imageId":"4", "uri":"/SriSriRaviShankar.jpeg" }
     ];
 
 var quote = quotes[Math.floor(Math.random()*quotes.length)];
+
+
+var image = images[Math.floor(Math.random()*images.length)];
+
+
 document.getElementById("quote").innerHTML =
 '<p class="quote">' +quote.quoteMessage + '</p>';
+
+document.body.style.backgroundImage = 'url('+ image.uri +')';
 })();
