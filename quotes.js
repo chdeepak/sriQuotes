b6b30c6c825dedf32e5c49d0a961818735743b8d
Deepak <chdeepak@thoughtworks.com>
2016-05-31 23:59:37 +0530

    css changes

--- quotes.js
+++ quotes.js
@@ -2,7 +2,12 @@
 var quotes = [
     { "quoteId":"1", "quoteMessage":"The only thing you must remember is how fortunate you are. When you forget this, you become sad" },
     { "quoteId":"2", "quoteMessage":"This definite knowledge that I am not the body, I am the Self, I am the space, I am the Imperishable, untouched, untainted by the \"prakriti,\" and by this world around me; this body is all hollow and empty, and every particle in this body is changing, and changing, and changing; the mind is changing and changing and changing---this definite knowledge is the way out of the cycle." },
-    { "quoteId":"3", "quoteMessage":"When you realise everyone is a part of your Self, you enjoy the whole world without a sense of craving" }
+    { "quoteId":"3", "quoteMessage":"When you realise everyone is a part of your Self, you enjoy the whole world without a sense of craving" },
+    { "quoteId":"4", "quoteMessage":"When we get in touch with the source of life then all other tensions, worries fall off and we can sing, dance and be happy. Life can become very intense, intense in the sense of being joyful." },
+    { "quoteId":"5", "quoteMessage":"Your bondage to the Master, to the Truth, to the ancient Knowledge of the Rishis is your saviour." },
+    { "quoteId":"6", "quoteMessage":"The mind looks for something new. The heart longs for the old. Life is a blend of both."},
+    { "quoteId":"7", "quoteMessage":"Use anger as a tool but don’t be a tool of anger."},
+    { "quoteId":"8", "quoteMessage":"Just wake up and see that you are beautiful. The innocence in you is so beautiful. The dance in you is so beautiful."}
     ];
   var images = [
     { "imageId":"1", "uri":"/srisri.jpeg" },
