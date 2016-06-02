73cfe6662d924cefa174c8e8cf889bf8d93f1e66
Deepak <chdeepak@thoughtworks.com>
2016-06-02 14:08:33 +0530

    images

--- mostvisited.js
+++ mostvisited.js
@@ -1,34 +1,21 @@
-function onAnchorClick(event) {
-  chrome.tabs.create({ url: event.srcElement.href });
-  return false;
-}
 
-// Given an array of URLs, build a DOM list of these URLs in the
-// browser action popup.
-function buildPopupDom(mostVisitedURLs) {
-  // var popupDiv = document.getElementById('topSitesMenu');
-  // var ol = popupDiv.appendChild(document.createElement('ol'));
-  var urlNumb = "";
+function createSideBar(mostVisitedURLs) {
+  var urlsCount = "";
 
   if(mostVisitedURLs.length < 10)
-    urlNumb = mostVisitedURLs.length;
+    urlsCount = mostVisitedURLs.length;
   else
-    urlNumb = 10;
+    urlsCount = 10;
 
-  for (var i = 0; i < urlNumb; i++) {
-    // var li = ol.appendChild(document.createElement('li'));
-    // var a = li.appendChild(document.createElement('a'));
+  for (var i = 0; i < urlsCount; i++) {
     var link = mostVisitedURLs[i].url;
     var title = mostVisitedURLs[i].title;
     if(title.length >35){
       title= title.substring(0,32);
       title.concat('...');
     }
-    // $(a).append('<img src="https://plus.google.com/_/favicon?domain_url='+a.href+'">');
     $('#sidebar ol').append('<a href="'+link+'">'+'<li><div style="color:black"><img src="https://plus.google.com/_/favicon?domain_url='+link+'">      '+title+'</div></li></a>');
-    // a.appendChild(document.createTextNode(mostVisitedURLs[i].title));
-    // a.addEventListener('click', onAnchorClick);
   }
 }
 
-chrome.topSites.get(buildPopupDom);
+chrome.topSites.get(createSideBar);
