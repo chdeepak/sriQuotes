1182ce744f660c49f5d432013c44e2d1c08098bf
Deepak <chdeepak@thoughtworks.com>
2016-06-10 17:49:10 +0530

    add feedback

--- quotes.js
+++ quotes.js
@@ -1,154 +1,160 @@
 (function() {
     var quotes = [{
         "quoteid": 1,
-        "quotemessage": "The only thing you must remember is how fortunate you are. When you forget this, you become sad."
-    }, {
-        "quoteid": 2,
-        "quotemessage": "Your bondage to the Master, to the Truth, to the ancient Knowledge of the Rishis is your saviour."
-    }, {
-        "quoteid": 3,
-        "quotemessage": "When you realise everyone is a part of your Self, you enjoy the whole world without a sense of craving."
-    }, {
-        "quoteid": 4,
-        "quotemessage": "The mind looks for something new. The heart longs for the old. Life is a blend of both."
-    }, {
-        "quoteid": 5,
-        "quotemessage": "Use anger as a tool but don't be a tool of anger."
-    }, {
-        "quoteid": 6,
-        "quotemessage": "One who is in love with the entire Universe worships everything in this Universe"
-    }, {
-        "quoteid": 7,
-        "quotemessage": "The power of spiritual knowledge gives you centeredness, which brings out passion in work & dispassion in meditation."
-    }, {
-        "quoteid": 8,
-        "quotemessage": "Just wake up and see that you are beautiful. The innocence in you is so beautiful. The dance in you is so beautiful."
-    }, {
-        "quoteid": 9,
-        "quotemessage": "When your intentions are very pure and clear, nature brings support to you."
-    }, {
-        "quoteid": 10,
-        "quotemessage": "Wherever there is sincerity & talent, people do recognize them. It may take some time but we should have some patience and hold on to our passion."
-    }, {
-        "quoteid": 11,
-        "quotemessage": "Do not be feverish about success, if your aim is clear and you have patience to move towards it, nature will support you."
-    }, {
-        "quoteid": 12,
-        "quotemessage": "Don't be over complacent or too feverish, take a middle path."
-    }, {
-        "quoteid": 13,
-        "quotemessage": "Recognize and honour your uniqueness."
-    }, {
-        "quoteid": 14,
-        "quotemessage": "Spiritual knowledge improves intuitive ability, innovative ability and communication."
-    }, {
-        "quoteid": 15,
-        "quotemessage": "The positive aspect of competition in a business scenario is it helps you to be more alert and innovative."
-    }, {
-        "quoteid": 16,
-        "quotemessage": "Keeping the mind open to new ideas, not being too anxious about success, putting 100% effort and meditation is the formula for entrepreneurs."
-    }, {
-        "quoteid": 17,
-        "quotemessage": "Love is not an emotion. It is your very existence."
-    }, {
-        "quoteid": 18,
-        "quotemessage": "Faith is realizing that you always get what you need."
-    }, {
-        "quoteid": 19,
-        "quotemessage": "Today is a gift from God - that is why it is called the present."
-    }, {
-        "quoteid": 20,
-        "quotemessage": "When feverishness clogs your head, your mind is not clear; poetry cannot dawn on that. If there is feverishness, any creative thought will not come."
-    }, {
-        "quoteid": 21,
-        "quotemessage": "If you can win over your mind, you can win over the whole world."
-    }, {
-        "quoteid": 22,
-        "quotemessage": "Life works on strange laws of nature (Karma). One never knows when a friend turns enemy & vice-versa. Rely on your Self; self-reliance"
-    }, {
-        "quoteid": 23,
-        "quotemessage": "Why can't we control our anger? because we love perfection. Make a little room for imperfection in our lives."
-    }, {
-        "quoteid": 24,
-        "quotemessage": "If something can bring you great pleasure, it can also bring you pain."
-    }, {
-        "quoteid": 25,
-        "quotemessage": "Don't Fall in love, Rise in Love!"
-    }, {
-        "quoteid": 26,
-        "quotemessage": "There is no use getting angry at something that has already happened.All you can do is your best to check it doesn't happen again."
-    }, {
-        "quoteid": 27,
-        "quotemessage": "Nothing in the world can bother you as much as your own mind, I tell you. In fact, others seem to be bothering you, but it is not other, it is your own mind."
-    }, {
-        "quoteid": 28,
-        "quotemessage": "Infinity means it is permeating all the finite things"
-    }, {
-        "quoteid": 29,
-        "quotemessage": "The path of love is not a tedious path. It's a path of joy. It's a path of singing and dancing."
-    }, {
-        "quoteid": 30,
-        "quotemessage": "Joy is never tomorrow; it is always now."
-    }, {
-        "quoteid": 31,
-        "quotemessage": "Life is a ball we should play with it !"
-    }, {
-        "quoteid": 32,
-        "quotemessage": "You need to see your life in context of time and space — how vast creation is, how big the universe is and how small your life is, relative"
-    }, {
-        "quoteid": 33,
-        "quotemessage": "If someone blames you directly, do not believe it. Just know that they are taking away your bad karma and let it"
-    }, {
-        "quoteid": 34,
-        "quotemessage": "Your desire for pleasure or happiness makes you unhappy."
-    }, {
-        "quoteid": 35,
-        "quotemessage": "Healthy breathing is the key to eliminating these toxins and harmonising emotions. Neither"
-    }, {
-        "quoteid": 36,
-        "quotemessage": "Shasana means rules someone else imposes on you. Anushasana means rules you impose on yourself."
-    }, {
-        "quoteid": 37,
-        "quotemessage": "Drop all your defenses. Anyone can make a mistake - even you. Do not defend your mistakes; just accept them and move on. When you are totally defenseless, that is when you will be completely strong."
-    }, {
-        "quoteid": 38,
-        "quotemessage": "We should not exit from this world without finding out who we are and where we have come from. This is very important."
-    }, {
-        "quoteid": 39,
-        "quotemessage": "Forgetfulness of the infinite is misery. Forgetfulness of the trivial is ecstasy."
-    }, {
-        "quoteid": 40,
-        "quotemessage": "Only the heart can speak or hear that which is authentic."
-    }, {
-        "quoteid": 41,
-        "quotemessage": "Everyone is like one's own finger; everyone is an inseparable part of the Being. There is no separateness; there is no “two”. The entire creation is one Whole.\n"
-    }, {
-        "quoteid": 42,
-        "quotemessage": "You are being loved very dearly. This reminder softens you; brings about miracles around you and changes your life"
-    }, {
-        "quoteid": 43,
-        "quotemessage": "The more the witness grows in you, the more playful and untouched you are"
-    }, {
-        "quoteid": 44,
-        "quotemessage": "f you want to grow in Divine Love, you have to drop the pride and all the artificial walls you build between yourself and others. Through these walls, we keep judging others, and we think others are judging us."
-    }, {
-        "quoteid": 45,
-        "quotemessage": "Spirituality is nothing but caring and sharing; offering oneself in service"
-    }, {
-        "quoteid": 46,
-        "quotemessage": "Source of action is thought, source of thought is your Self. Whatever thought you encourage about yourself, that’s the direction taken"
-    }, {
-        "quoteid": 47,
         "quotemessage": "A Guru will put you into your Self, make you reach the goal - not just tell you some techniques and show you some path."
     }, {
-        "quoteid": 48,
+        "quoteid": 2,
         "quotemessage": "Curiosity is the mother of science; Wonder is the mother of spirituality"
     }, {
-        "quoteid": 49,
+        "quoteid": 3,
+        "quotemessage": "Do not be feverish about success, if your aim is clear and you have patience to move towards it, nature will support you."
+    }, {
+        "quoteid": 4,
+        "quotemessage": "Don't be over complacent or too feverish, take a middle path."
+    }, {
+        "quoteid": 5,
+        "quotemessage": "Don't Fall in love, Rise in Love!"
+    }, {
+        "quoteid": 6,
+        "quotemessage": "Drop all your defenses. Anyone can make a mistake - even you. Do not defend your mistakes; just accept them and move on. When you are totally defenseless, that is when you will be completely strong."
+    }, {
+        "quoteid": 7,
+        "quotemessage": "Everyone is like one's own finger; everyone is an inseparable part of the Being. There is no separateness; there is no \"two\". The entire creation is one Whole."
+    }, {
+        "quoteid": 8,
+        "quotemessage": "Faith is realizing that you always get what you need."
+    }, {
+        "quoteid": 9,
+        "quotemessage": "Forgetfulness of the infinite is misery. Forgetfulness of the trivial is ecstasy."
+    }, {
+        "quoteid": 10,
+        "quotemessage": "Healthy breathing is the key to eliminating these toxins and harmonising emotions. Neither"
+    }, {
+        "quoteid": 11,
+        "quotemessage": "If someone blames you directly, do not believe it. Just know that they are taking away your bad karma and let it"
+    }, {
+        "quoteid": 12,
+        "quotemessage": "If something can bring you great pleasure, it can also bring you pain."
+    }, {
+        "quoteid": 13,
+        "quotemessage": "If you can win over your mind, you can win over the whole world."
+    }, {
+        "quoteid": 14,
+        "quotemessage": "If you want to grow in Divine Love, you have to drop the pride and all the artificial walls you build between yourself and others. Through these walls, we keep judging others, and we think others are judging us."
+    }, {
+        "quoteid": 15,
+        "quotemessage": "Infinity means it is permeating all the finite things"
+    }, {
+        "quoteid": 16,
+        "quotemessage": "Joy is never tomorrow; it is always now."
+    }, {
+        "quoteid": 17,
+        "quotemessage": "Just wake up and see that you are beautiful. The innocence in you is so beautiful. The dance in you is so beautiful."
+    }, {
+        "quoteid": 18,
+        "quotemessage": "Keeping the mind open to new ideas, not being too anxious about success, putting 100% effort and meditation is the formula for entrepreneurs."
+    }, {
+        "quoteid": 19,
+        "quotemessage": "Life is a ball we should play with it !"
+    }, {
+        "quoteid": 20,
+        "quotemessage": "Life works on strange laws of nature (Karma). One never knows when a friend turns enemy & vice-versa. Rely on your Self; self-reliance"
+    }, {
+        "quoteid": 21,
+        "quotemessage": "Love is not an emotion. It is your very existence."
+    }, {
+        "quoteid": 22,
         "quotemessage": "Love is that phenomenon of dissolving, disappearing, merging, becoming one with the infinite. Love is that phenomenon of total letting go."
     }, {
-        "quoteid": 50,
+        "quoteid": 23,
+        "quotemessage": "Nothing in the world can bother you as much as your own mind, I tell you. In fact, others seem to be bothering you, but it is not other, it is your own mind."
+    }, {
+        "quoteid": 24,
+        "quotemessage": "One who is in love with the entire Universe worships everything in this Universe"
+    }, {
+        "quoteid": 25,
+        "quotemessage": "Only the heart can speak or hear that which is authentic."
+    }, {
+        "quoteid": 26,
+        "quotemessage": "Recognize and honour your uniqueness."
+    }, {
+        "quoteid": 27,
+        "quotemessage": "Shasana means rules someone else imposes on you. Anushasana means rules you impose on yourself."
+    }, {
+        "quoteid": 28,
+        "quotemessage": "Source of action is thought, source of thought is your Self. Whatever thought you encourage about yourself, that’s the direction taken"
+    }, {
+        "quoteid": 29,
+        "quotemessage": "Spiritual knowledge improves intuitive ability, innovative ability and communication."
+    }, {
+        "quoteid": 30,
+        "quotemessage": "Spirituality is nothing but caring and sharing; offering oneself in service"
+    }, {
+        "quoteid": 31,
+        "quotemessage": "The mind looks for something new. The heart longs for the old. Life is a blend of both."
+    }, {
+        "quoteid": 32,
+        "quotemessage": "The more the witness grows in you, the more playful and untouched you are"
+    }, {
+        "quoteid": 33,
+        "quotemessage": "The only thing you must remember is how fortunate you are. When you forget this, you become sad."
+    }, {
+        "quoteid": 34,
+        "quotemessage": "The path of love is not a tedious path. It's a path of joy. It's a path of singing and dancing."
+    }, {
+        "quoteid": 35,
+        "quotemessage": "The positive aspect of competition in a business scenario is it helps you to be more alert and innovative."
+    }, {
+        "quoteid": 36,
+        "quotemessage": "The power of spiritual knowledge gives you centeredness, which brings out passion in work & dispassion in meditation."
+    }, {
+        "quoteid": 37,
+        "quotemessage": "There are moments when your body, mind and breath are in harmony. That is when you achieve yoga."
+    }, {
+        "quoteid": 38,
+        "quotemessage": "There is no use getting angry at something that has already happened.All you can do is your best to check it doesn't happen again."
+    }, {
+        "quoteid": 39,
+        "quotemessage": "Today is a gift from God - that is why it is called the present."
+    }, {
+        "quoteid": 40,
+        "quotemessage": "Use anger as a tool but don't be a tool of anger."
+    }, {
+        "quoteid": 41,
+        "quotemessage": "We should not exit from this world without finding out who we are and where we have come from. This is very important."
+    }, {
+        "quoteid": 42,
+        "quotemessage": "When feverishness clogs your head, your mind is not clear; poetry cannot dawn on that. If there is feverishness, any creative thought will not come."
+    }, {
+        "quoteid": 43,
+        "quotemessage": "When you know your strength, with skill and intelligence, you can turn every disadvantage into an advantage."
+    }, {
+        "quoteid": 44,
+        "quotemessage": "When you realise everyone is a part of your Self, you enjoy the whole world without a sense of craving."
+    }, {
+        "quoteid": 45,
+        "quotemessage": "When your intentions are very pure and clear, nature brings support to you."
+    }, {
+        "quoteid": 46,
+        "quotemessage": "Wherever there is sincerity & talent, people do recognize them. It may take some time but we should have some patience and hold on to our passion."
+    }, {
+        "quoteid": 47,
+        "quotemessage": "Why can't we control our anger? because we love perfection. Make a little room for imperfection in our lives."
+    }, {
+        "quoteid": 48,
+        "quotemessage": "You are being loved very dearly. This reminder softens you; brings about miracles around you and changes your life"
+    }, {
+        "quoteid": 49,
         "quotemessage": "You are free right now."
+    }, {
+        "quoteid": 50,
+        "quotemessage": "You need to see your life in context of time and space - how vast creation is, how big the universe is and how small your life is"
+    }, {
+        "quoteid": 51,
+        "quotemessage": "Your bondage to the Master, to the Truth, to the ancient Knowledge of the Rishis is your saviour."
+    }, {
+        "quoteid": 52,
+        "quotemessage": "Your desire for pleasure or happiness makes you unhappy."
     }];
 
 
