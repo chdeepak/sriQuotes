ba738776348555aaabdbad07f792b974c5ac1977
Deepak <chdeepak@thoughtworks.com>
2016-06-10 18:32:26 +0530

    add feedback

--- js/quotes.js
+++ js/quotes.js
@@ -1,17 +1,18 @@
 (function() {
 
-
     var quotes = new Array();
     var images = new Array();
 
     $.when($.getJSON('./../json/quotes.json'), $.getJSON('./../json/images.json'))
         .done(function(quotesFile, photosFile) {
             quotes = quotesFile[0].quotes;
-
             images = photosFile[0].photos;
-            var quote = quotes[Math.floor(Math.random() * quotes.length)];
 
-            var image = images[Math.floor(Math.random() * images.length)];
+            var quoteIndex = Math.floor(Math.random() * quotes.length);
+            var quote = quotes[quoteIndex];
+
+            var imageIndex = Math.floor(Math.random() * images.length)
+            var image = images[imageIndex];
 
             document.getElementById("quote").innerHTML = '<p class="quote">' + quote.quotemessage + '</p>';
 
